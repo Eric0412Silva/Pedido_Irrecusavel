d573d862a7073f7a0755babaa598b06cb7199cbb
Eric José <112144580+Eric0412Silva@users.noreply.github.com>
2023-09-19 17:55:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ let button = document.getElementById('no');
         button.style.top = Math.random() * height + "px";
         button.style.left = Math.random() * width + "px";
     })
+
+// Função que será executada quando o botão for clicado
+        document.getElementById("botaoSim").addEventListener("click", function() {
+            // Define a mensagem desejada
+            var mensagem = "Minha chave pix é celular (88)9.8145-6310";
+            
+            // Exibe a mensagem no elemento <span>
+            document.getElementById("mensagemPix").textContent = mensagem;
+        });
